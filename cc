62168630c3c58b523bad0f5085b65728f417da6e
Ahmad Kamal Anwar <ahmad.kamalanwar23@gmail.com>
2023-06-08 13:01:56 +0500

    Initial setup

---
+++ cc
@@ -0,0 +1,1 @@
+docker-compose exec -u nginx ins_web sh -c "bin/console c:c"
---
+++ docker/provision/mysql/init/01-databases.sql
@@ -0,0 +1,9 @@
+-- noinspection SqlDialectInspectionForFile
+-- noinspection SqlNoDataSourceInspectionForFile
+
+# create databases
+CREATE DATABASE IF NOT EXISTS `new_aggregator`;
+
+# create root user and grant rights
+# CREATE USER 'root'@'localhost' IDENTIFIED BY 'local';
+# GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
