f22ca21ac132553b19fbd662387dad00e9b63f0d
Ahmad Kamal Anwar <ahmad.kamalanwar23@gmail.com>
2023-06-08 23:26:03 +0500

    Improving code

--- docker/provision/mysql/init/01-databases.sql
+++ docker/provision/mysql/init/01-databases.sql
@@ -2,7 +2,7 @@
 -- noinspection SqlNoDataSourceInspectionForFile
 
 # create databases
-CREATE DATABASE IF NOT EXISTS `new_aggregator`;
+CREATE DATABASE IF NOT EXISTS `news_aggregator`;
 
 # create root user and grant rights
 # CREATE USER 'root'@'localhost' IDENTIFIED BY 'local';
